ef85f3c181b4ae35690a1f1b3825e0c07fed2516
Jonas Mitchell <JonasBenjaminMitchell@gmail.com>
2024-03-27 14:34:26 -0400

    add solution for MergeSortedArrays

---
+++ LeetCodeDotNet/LeetCodeConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace LeetCodeConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MergeSortedArray();
+        }
+
+        public static void MergeSortedArray()
+        {
+            int[] nums1 = {-1,0,0,3,3,3,0,0,0}; 
+            int[] nums2 = {1, 2, 2}; 
+            int m = 6, n = 3;
+            // [-1,0,0,1,2,2,3,3,3]
+            // int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            // int[] nums2 = { 2, 5, 6 };
+            // int m = 3, n = 3;
+
+
+            var lib = new LeetCodeLibrary.Easy.MergeSortedArray();
+            lib.Merge(nums1, m, nums2, n);
+            var foo = "[" + string.Join(",", nums1) + "]";
+            Console.Write(foo);
+        }
+    }
+}
---
+++ LeetCodeDotNet/LeetCodeLibraryTests/Easy/MergeSortedArrayTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeLibrary.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary.Easy.Tests
+{
+    [TestClass()]
+    public class MergeSortedArrayTests
+    {
+        [TestMethod()]
+        public void MergeTest1()
+        {
+            int[] nums1 = { -1, 0, 0, 3, 3, 3, 0, 0, 0 };
+            int[] nums2 = { 1, 2, 2 };
+            int m = 6, n = 3;
+            var lib = new MergeSortedArray();
+            lib.Merge(nums1, m, nums2, n);
+            CollectionAssert.AreEqual(new int[] { -1, 0, 0, 1, 2, 2, 3, 3, 3 }, nums1);
+
+        }
+        [TestMethod()]
+        public void MergeTest2()
+        {
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = { 2, 5, 6 };
+            int m = 3, n = 3;
+            var lib = new MergeSortedArray();
+            lib.Merge(nums1, m, nums2, n);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 2, 3, 5, 6 }, nums1);
+        }
+    }
+}
