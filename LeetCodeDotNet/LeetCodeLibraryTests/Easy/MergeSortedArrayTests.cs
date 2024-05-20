aa2eec186f4d669b25421439c1abd3e16bb3074e
Jonas Mitchell <JonasBenjaminMitchell@gmail.com>
2024-05-20 15:12:49 -0400

    merge merge sorted array tests

--- LeetCodeDotNet/LeetCodeLibraryTests/Easy/MergeSortedArrayTests.cs
+++ LeetCodeDotNet/LeetCodeLibraryTests/Easy/MergeSortedArrayTests.cs
@@ -11,58 +11,18 @@ namespace LeetCodeLibrary.Easy.Tests
     [TestClass()]
     public class MergeSortedArrayTests
     {
-        [TestMethod()]
-        public void MergeTest1()
+        [TestMethod]
+        [DataRow(new [] { -1, 0, 0, 3, 3, 3, 0, 0, 0 }, new [] { 1, 2, 2 }, 6, 3, new [] { -1, 0, 0, 1, 2, 2, 3, 3, 3 })]
+        [DataRow(new [] { 1, 2, 3, 0, 0, 0 }, new [] { 2, 5, 6 }, 3, 3, new [] { 1, 2, 2, 3, 5, 6 })]
+        [DataRow(new [] { 1 }, new int[] { }, 1, 0, new [] { 1 })]
+        [DataRow(new [] { 0 }, new [] { 1 }, 0, 1, new [] { 1 })]
+        [DataRow(new [] { 1, 0 }, new [] { 2 }, 1, 1, new [] { 1, 2 })]
+        public void MergeTest1(int[] nums1, int[] nums2, int m, int n, int[] expected)
         {
-            int[] nums1 = { -1, 0, 0, 3, 3, 3, 0, 0, 0 };
-            int[] nums2 = { 1, 2, 2 };
-            int m = 6, n = 3;
             var lib = new MergeSortedArray();
             lib.Merge(nums1, m, nums2, n);
-            CollectionAssert.AreEqual(new int[] { -1, 0, 0, 1, 2, 2, 3, 3, 3 }, nums1);
-
-        }
-        [TestMethod()]
-        public void MergeTest2()
-        {
-            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
-            int[] nums2 = { 2, 5, 6 };
-            int m = 3, n = 3;
-            var lib = new MergeSortedArray();
-            lib.Merge(nums1, m, nums2, n);
-            CollectionAssert.AreEqual(new int[] { 1, 2, 2, 3, 5, 6 }, nums1);
+            CollectionAssert.AreEqual(expected, nums1);
         }
 
-        [TestMethod()]
-        public void MergeTest3()
-        {
-            int[] nums1 = { 1 };
-            int[] nums2 = { };
-            int m = 1, n = 0;
-            var lib = new MergeSortedArray();
-            lib.Merge(nums1, m, nums2, n);
-            CollectionAssert.AreEqual(new int[] { 1 }, nums1);
-        }
-
-        [TestMethod()]
-        public void MergeTest4()
-        {
-            int[] nums1 = { 0 };
-            int[] nums2 = { 1 };
-            int m = 0, n = 1;
-            var lib = new MergeSortedArray();
-            lib.Merge(nums1, m, nums2, n);
-            CollectionAssert.AreEqual(new int[] { 1 }, nums1);
-        }
-        [TestMethod()]
-        public void MergeTest5()
-        {
-            int[] nums1 = { 1, 0 };
-            int[] nums2 = { 2 };
-            int m = 1, n = 1;
-            var lib = new MergeSortedArray();
-            lib.Merge(nums1, m, nums2, n);
-            CollectionAssert.AreEqual(new int[] { 1, 2 }, nums1);
-        }
     }
 }
