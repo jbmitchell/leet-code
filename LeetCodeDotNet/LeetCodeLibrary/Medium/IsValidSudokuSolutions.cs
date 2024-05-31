5248a3dfa246ef74877715eced69db9f692aa422
Jonas Mitchell <JonasBenjaminMitchell@gmail.com>
2024-05-30 20:40:06 -0400

    Add solution for Valid Sudoku

---
+++ LeetCodeDotNet/LeetCodeLibrary/Medium/IsValidSudokuSolutions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary.Medium
+{
+    public class IsValidSudokuSolutions
+    {
+        //https://leetcode.com/problems/valid-sudoku/description/
+        //board.length == 9
+        //board[i].length == 9
+        //board[i][j] is a digit 1-9 or '.'.
+        public bool IsValidSudoku(char[][] board)
+        {
+            //check rows
+            var i = 0;
+            while (i < 9)
+            {
+                var foundRow = new HashSet<int>();
+                var j = 0;
+                while (j < 9)
+                {
+                    var cell = (int)char.GetNumericValue(board[i][j]);
+                    if (cell == -1)
+                    {
+                        j++;
+                        continue;
+                    }
+                    if (foundRow.Contains(cell)) return false;
+                    foundRow.Add(cell);
+                    j++;
+                }
+                i++;
+            }
+
+            //check cols
+            i = 0;
+            while (i < 9)
+            {
+                var foundCol = new HashSet<int>();
+                var j = 0;
+                while (j < 9)
+                {
+                    var cell = (int)char.GetNumericValue(board[j][i]);
+                    if (cell == -1)
+                    {
+                        j++;
+                        continue;
+                    }
+                    if (foundCol.Contains(cell)) return false;
+                    foundCol.Add(cell);
+                    j++;
+                }
+                i++;
+            }
+            var result = CheckSubSquare(0, 0, board);
+            if (!result) return false;
+            result = CheckSubSquare(0, 3, board);
+            if (!result) return false;
+            result = CheckSubSquare(0, 6, board);
+            if (!result) return false;
+            result = CheckSubSquare(3, 0, board);
+            if (!result) return false;
+            result = CheckSubSquare(3, 3, board);
+            if (!result) return false;
+            result = CheckSubSquare(3, 6, board);
+            if (!result) return false;
+            result = CheckSubSquare(6, 0, board);
+            if (!result) return false;
+            result = CheckSubSquare(6, 3, board);
+            if (!result) return false;
+            result = CheckSubSquare(6, 6, board);
+            return result;
+        }
+
+        private static bool CheckSubSquare(int startY, int startX, char[][] board)
+        {
+            var found = new HashSet<int>();
+            var i = startY;
+            while (i < startY + 3)
+            {
+                var j = startX;
+                while (j < startX + 3)
+                {
+                    var cell = (int)char.GetNumericValue(board[i][j]);
+                    if (cell == -1)
+                    {
+                        j++;
+                        continue;
+                    }
+                    if (found.Contains(cell)) return false;
+                    found.Add(cell);
+                    j++;
+                }
+                i++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeDotNet/LeetCodeLibraryTests/Medium/IsValidSudokuSolutionsTests.cs
@@ -0,0 +1,99 @@
+﻿using LeetCodeLibrary.Medium;
+
+namespace LeetCodeLibraryTests.Medium
+{
+    [TestClass()]
+    public class IsValidSudokuSolutionsTests
+    {
+        [TestMethod()]
+        public void IsValidSudokuTest1()
+        {
+            var board = new[] {
+                new[]{'5','3','.','.','7','.','.','.','.'},
+                new[]{'6','.','.','1','9','5','.','.','.'},
+                new[]{'.','9','8','.','.','.','.','6','.'},
+                new[]{'8','.','.','.','6','.','.','.','3'},
+                new[]{'4','.','.','8','.','3','.','.','1'},
+                new[]{'7','.','.','.','2','.','.','.','6'},
+                new[]{'.','6','.','.','.','.','2','8','.'},
+                new[]{'.','.','.','4','1','9','.','.','5'},
+                new[]{'.','.','.','.','8','.','.','7','9'}
+            };
+            //arrange
+            var solution = new IsValidSudokuSolutions();
+            //act
+            var result = solution.IsValidSudoku(board);
+
+            //assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod("Bad Col")]
+        public void IsValidSudokuTest2()
+        {
+            var board = new[] {
+                new[]{'8','3','.','.','7','.','.','.','.'},
+                new[]{'6','.','.','1','9','5','.','.','.'},
+                new[]{'.','9','8','.','.','.','.','6','.'},
+                new[]{'8','.','.','.','6','.','.','.','3'},
+                new[]{'4','.','.','8','.','3','.','.','1'},
+                new[]{'7','.','.','.','2','.','.','.','6'},
+                new[]{'.','6','.','.','.','.','2','8','.'},
+                new[]{'.','.','.','4','1','9','.','.','5'},
+                new[]{'.','.','.','.','8','.','.','7','9'}
+            };
+            //arrange
+            var solution = new IsValidSudokuSolutions();
+            //act
+            var result = solution.IsValidSudoku(board);
+
+            //assert
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod("Bad Row")]
+        public void IsValidSudokuTest3()
+        {
+            var board = new[] {
+                new[]{'5','3','.','.','7','.','.','.','.'},
+                new[]{'6','.','.','1','9','5','.','.','.'},
+                new[]{'.','9','8','.','.','.','.','6','.'},
+                new[]{'8','.','.','.','6','.','.','.','3'},
+                new[]{'4','.','.','8','.','3','.','.','1'},
+                new[]{'7','.','.','.','2','.','.','.','6'},
+                new[]{'.','6','.','.','.','.','2','8','.'},
+                new[]{'.','.','.','4','1','9','.','.','5'},
+                new[]{'9','.','.','.','8','.','.','7','9'}
+            };
+            //arrange
+            var solution = new IsValidSudokuSolutions();
+            //act
+            var result = solution.IsValidSudoku(board);
+
+            //assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod("Bad Sub Square")]
+        public void IsValidSudokuTest4()
+        {
+            var board = new[] {
+                new[]{'5','3','.','.','7','.','.','.','.'},
+                new[]{'6','.','.','1','9','5','.','.','.'},
+                new[]{'.','9','8','.','.','.','.','6','.'},
+                new[]{'8','.','.','.','6','.','.','.','3'},
+                new[]{'4','.','.','8','.','3','.','.','1'},
+                new[]{'7','.','.','.','2','.','.','.','6'},
+                new[]{'.','6','.','.','.','.','9','8','.'},
+                new[]{'.','.','.','4','1','9','.','.','5'},
+                new[]{'9','.','.','.','8','.','.','7','9'}
+            };
+            //arrange
+            var solution = new IsValidSudokuSolutions();
+            //act
+            var result = solution.IsValidSudoku(board);
+
+            //assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
